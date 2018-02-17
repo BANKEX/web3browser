2200bcc827fbd54e49260e6571bcd9a584b11f1d
Korovkina, Ekaterina (Agoda) <ekaterina.korovkina@agoda.com>
2018-02-17 12:01:03 +0700

    Guards are better, trust me

--- web3swiftBrowser/ViewController.swift
+++ web3swiftBrowser/ViewController.swift
@@ -13,6 +13,13 @@ import web3swift
 import WKBridge
 import AwaitKit
 
+enum JSRequestsError: String {
+    case noParamaters = "No parameters provided"
+    case notEnoughParameters = "Not enough parameters provided"
+    case accountOrDataIsInvalid = "Account or data is invalid"
+    case dataIsInvalid = ""
+}
+
 class BrowserViewController: UIViewController {
     
     enum Method: String {
@@ -35,7 +42,6 @@ class BrowserViewController: UIViewController {
         )
         webView.allowsBackForwardNavigationGestures = true
         webView.scrollView.isScrollEnabled = true
-        webView.navigationDelegate = self
         webView.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
         return webView
     }()
@@ -55,14 +61,6 @@ class BrowserViewController: UIViewController {
         } else {
             NSLog("web3.js not found in bundle")
         }
-        
-//        js +=
-//        """
-//        let web3 = new Web3(new Web3.providers.HttpProvider("\(session.config.rpcURL.absoluteString)"));
-//        web3.eth.defaultAccount = "\(session.account.address.address)"
-//        window.web3 = web3
-//        """
-        
         let userScript = WKUserScript(source: js, injectionTime: .atDocumentStart, forMainFrameOnly: false)
         config.userContentController.addUserScript(userScript)
         return config
@@ -82,92 +80,74 @@ class BrowserViewController: UIViewController {
             ])
         
         webView.load(URLRequest(url: URL(string: "https://ownanumber.github.io/")!))
-        
-//        async {
-            do {
-                let userDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-                let keystoreManager = KeystoreManager.managerForPath(userDir + "/keystore")
-                var ks: EthereumKeystoreV3?
-                if (keystoreManager?.addresses?.count == 0) {
-                    ks = try EthereumKeystoreV3(password: "BANKEXFOUNDATION")
-                    let keydata = try JSONEncoder().encode(ks!.keystoreParams)
-                    FileManager.default.createFile(atPath: userDir + "/keystore"+"/key.json", contents: keydata, attributes: nil)
+        do {
+            let userDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            let keystoreManager = KeystoreManager.managerForPath(userDir + "/keystore")
+            var ks: EthereumKeystoreV3?
+            if (keystoreManager?.addresses?.count == 0) {
+                ks = try EthereumKeystoreV3(password: "BANKEXFOUNDATION")
+                let keydata = try JSONEncoder().encode(ks!.keystoreParams)
+                FileManager.default.createFile(atPath: userDir + "/keystore"+"/key.json", contents: keydata, attributes: nil)
+            }
+            guard let sender = keystoreManager?.addresses?.first else {return}
+            print(sender)
+            let web3 = Web3.InfuraRinkebyWeb3()
+            web3.addKeystoreManager(keystoreManager)
+            
+            self.webView.bridge.register({ (parameters, completion) in
+                let url = web3.provider.url.absoluteString
+                completion(.success(["rpcURL": url as Any]))
+            }, for: "getRPCurl")
+            
+            self.webView.bridge.register({ (parameters, completion) in
+                let allAccounts = web3.hookedFunctions.getAccounts()
+                completion(.success(["accounts": allAccounts as Any]))
+            }, for: "eth_getAccounts")
+            
+            self.webView.bridge.register({ (parameters, completion) in
+                let coinbase = web3.hookedFunctions.getCoinbase()
+                completion(.success(["coinbase": coinbase as Any]))
+            }, for: "eth_coinbase")
+            
+            self.webView.bridge.register({ (parameters, completion) in
+                guard let parameters = parameters,
+                    let payload = parameters["payload"] as? [String:Any] else {
+                        completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.noParamaters.rawValue)))
+                        return
                 }
-                guard let sender = keystoreManager?.addresses![0] else {return}
-                print(sender)
-                let web3 = Web3.InfuraRinkebyWeb3()
-                web3.addKeystoreManager(keystoreManager)
+                guard let personalMessage = payload["data"] as? String,
+                    let account = payload["from"] as? String else {
+                        completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.notEnoughParameters.rawValue)))
+                        return
+                }
+                let result = web3.hookedFunctions.personalSign(personalMessage, account: account)
+                if result == nil {
+                    completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.accountOrDataIsInvalid.rawValue)))
+                    return
+                }
+                completion(.success(["signedMessage": result as Any]))
+            }, for: "eth_sign")
+            
+            self.webView.bridge.register({ (parameters, completion) in
+                guard let parameters = parameters else {
+                    completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.noParamaters.rawValue)))
+                    return
+                }
                 
-                self.webView.bridge.register({ (parameters, completion) in
-                    let url = web3.provider.url.absoluteString
-                    completion(.success(["rpcURL": url as Any]))
-                }, for: "getRPCurl")
+                guard let transaction = parameters["transaction"] as? [String:Any] else {
+                    completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.notEnoughParameters.rawValue)))
+                    return
+                }
                 
-                self.webView.bridge.register({ (parameters, completion) in
-                    let allAccounts = web3.hookedFunctions.getAccounts()
-                    completion(.success(["accounts": allAccounts as Any]))
-                }, for: "eth_getAccounts")
-                
-                self.webView.bridge.register({ (parameters, completion) in
-                    let coinbase = web3.hookedFunctions.getCoinbase()
-                    completion(.success(["coinbase": coinbase as Any]))
-                }, for: "eth_coinbase")
-                self.webView.bridge.register({ (parameters, completion) in
-                    if parameters == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "No parameters provided")))
-                        return
-                    }
-                    let payload = parameters!["payload"] as? [String:Any]
-                    if payload == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "No parameters provided")))
-                        return
-                    }
-                    let personalMessage = payload!["data"] as? String
-                    let account = payload!["from"] as? String
-                    if personalMessage == nil || account == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "Not enough parameters provided")))
-                        return
-                    }
-                    let result = web3.hookedFunctions.personalSign(personalMessage!, account: account!)
-                    if result == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "Account or data is invalid")))
-                        return
-                    }
-                    completion(.success(["signedMessage": result as Any]))
-                }, for: "eth_sign")
-                self.webView.bridge.register({ (parameters, completion) in
-                    if parameters == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "No parameters provided")))
-                        return
-                    }
-                    let transaction = parameters!["transaction"] as? [String:Any]
-                    if transaction == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "Not enough parameters provided")))
-                        return
-                    }
-                    let result = web3.hookedFunctions.signTransaction(transaction!)
-                    if result == nil {
-                        completion(.failure(Bridge.JSError(code: 0, description: "Data is invalid")))
-                        return
-                    }
-                    completion(.success(["signedTransaction": result as Any]))
-                }, for: "eth_signTransaction")
-            }
-            catch{
-                print(error)
-            }
-//        }
-    }
-}
-
-extension BrowserViewController: WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        
-    }
-}
-
-extension BrowserViewController: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        NSLog("message \(message.body)")
+                guard let result = web3.hookedFunctions.signTransaction(transaction) else {
+                    completion(.failure(Bridge.JSError(code: 0, description: JSRequestsError.dataIsInvalid.rawValue)))
+                    return
+                }
+                completion(.success(["signedTransaction": result as Any]))
+            }, for: "eth_signTransaction")
+        }
+        catch{
+            print(error)
+        }
     }
 }
