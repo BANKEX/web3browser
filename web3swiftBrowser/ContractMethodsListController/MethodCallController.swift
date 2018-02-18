6b15088cf6402ffae1ed020d5d450b096bde1344
Ekaterina Korovkina <c.korovkina@gmail.com>
2018-02-18 12:17:44 +0700

    Can work with events and constructors

--- web3swiftBrowser/ContractMethodsListController/MethodCallController.swift
+++ web3swiftBrowser/ContractMethodsListController/MethodCallController.swift
@@ -16,12 +16,6 @@ class MethodCallController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -33,6 +27,12 @@ class MethodCallController: UITableViewController {
         switch abiToCall {
         case .function(let abiFunc):
             numberOfItems = (abiFunc.inputs.count == 0) ? 1 : abiFunc.inputs.count + 2
+        case .constructor(let constructor):
+            numberOfItems = (constructor.inputs.count == 0) ? 1 : constructor.inputs.count + 2
+        case .fallback(let fallback):
+            numberOfItems = 0
+        case .event(let event):
+            numberOfItems = (event.inputs.count == 0) ? 1 : event.inputs.count + 2
         default:
             return 0
         }
@@ -59,60 +59,15 @@ class MethodCallController: UITableViewController {
             switch abiToCall {
             case .function(let function):
                 cell.parameterNameLabel.text = function.inputs[indexPath.row - 1].name
-//            case .constructor(let construct):
-//            case .event(let event):
-//            case .fallback(let fallback):
+            case .constructor(let construct):
+                cell.parameterNameLabel.text = construct.inputs[indexPath.row - 1].name
+            case .event(let event):
+                cell.parameterNameLabel.text = event.inputs[indexPath.row - 1].name
             default:
                 cell.parameterNameLabel.text = ""
             }
             return cell
         }
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
